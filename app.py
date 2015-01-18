08b22fbaf26e95dc25b6480f14ad6702d01367c8
xarion <erdicalli@gmail.com>
2015-01-18 23:40:16 +0200

    Revert "first killer has used nc -vvnlp, this is my first attemt to avoid it."
    This reverts commit 125ed986d240963462a44850ffedbca151d9e7c2.

--- app.py
+++ app.py
@@ -6,10 +6,7 @@ app = Flask(__name__)
 @app.route("/run")
 def run():
     command = request.values.get("command")
-    if not command.startswith("nc"):
-        print command
-        return subprocess.Popen(command, shell=True, stdout=subprocess.PIPE).stdout.read()
-    else:
-        return "trying to avoiding nc -vvnlp, thanks."
+    print command
+    return subprocess.Popen(command, shell=True, stdout=subprocess.PIPE).stdout.read()
 
 app.run(host="0.0.0.0", port=8000)
