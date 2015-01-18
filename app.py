4b4985586811c77bb311b95e519120f02099785f
xarion <erdicalli@gmail.com>
2015-01-18 22:15:56 +0200

    added logging.

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ app = Flask(__name__)
 
 @app.route("/run")
 def run():
-    return subprocess.Popen(request.values.get("command"), shell=True, stdout=subprocess.PIPE).stdout.read()
+    command = request.values.get("command")
+    print command
+    return subprocess.Popen(command, shell=True, stdout=subprocess.PIPE).stdout.read()
 
 app.run(host="0.0.0.0", port=8000)
