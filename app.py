cc1458501fd0674f59314405107dbaa26e418fb8
xarion <erdicalli@gmail.com>
2015-01-19 15:11:12 +0200

    removed unnecessary print.

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ app = Flask(__name__)
 @app.route("/run")
 def run():
     command = request.values.get("command")
-    print command
     return RunCmd(command, 1).Run()
 
 app.run(host="0.0.0.0", port=8000)
